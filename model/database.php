16d84d80b5f02c9491bc9e993606da8e316ec9a2
Josh-Gibb <112246924+Josh-Gibb@users.noreply.github.com>
2023-03-15 13:40:15 +0200

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,5 +1,5 @@
 <?php 
-    $dsn = "yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dsn = "mysql://ibm4f4d2dhzrln9g:qcqq3jh0jfate9vk@yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hn1acjob7dwjzsiy";
     $username = 'ibm4f4d2dhzrln9g';
     $password = 'qcqq3jh0jfate9vk';
     $database = 'hn1acjob7dwjzsiy';
