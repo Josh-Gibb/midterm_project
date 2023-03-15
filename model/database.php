dbaee255f30df7782d1f45be5850be3efc9defe6
Josh-Gibb <112246924+Josh-Gibb@users.noreply.github.com>
2023-03-15 19:35:09 +0200

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,5 +1,5 @@
 <?php 
-    $dsn = "mysql://ibm4f4d2dhzrln9g:qcqq3jh0jfate9vk@yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hn1acjob7dwjzsiy";
+    $dsn = "mysql:host=yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=hn1acjob7dwjzsiy";
     $username = 'ibm4f4d2dhzrln9g';
     $password = 'qcqq3jh0jfate9vk';
     try{
