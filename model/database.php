ddf350d9fabc4517848041347780b1291775b359
Josh-Gibb <112246924+Josh-Gibb@users.noreply.github.com>
2023-03-15 17:11:18 +0200

    Update database.php

--- model/database.php
+++ model/database.php
@@ -2,7 +2,6 @@
     $dsn = "yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $username = 'ibm4f4d2dhzrln9g';
     $password = 'qcqq3jh0jfate9vk';
-    $database = 'hn1acjob7dwjzsiy';
     try{
         $db = new PDO($dsn, $username, $password);
     } catch(PDOException $e){
