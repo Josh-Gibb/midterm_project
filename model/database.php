e5ee146f1039fd0175c817a3ee448fc7831dffb7
Josh-Gibb <112246924+Josh-Gibb@users.noreply.github.com>
2023-03-15 13:36:05 +0200

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,9 +1,10 @@
 <?php 
     $dsn = "mysql:host=localhost; dbname=zippyusedautos";
-    $username = 'root';
+    $username = 'ibm4f4d2dhzrln9g';
+    $password = 'qcqq3jh0jfate9vk';
 
     try{
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch(PDOException $e){
         $error = "Database Error! " . $e->getMessage();
         include('view/error.php');
